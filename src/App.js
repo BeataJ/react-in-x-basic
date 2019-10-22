bee0f595719a765b9e04b80f5308bd55c54c64b1
beata <beata@emc2wave.com>
2019-10-22 16:04:23 -0700

    add html element to fragment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import TodoList from './TodoList'
+import React from "react";
+import TodoList from "./TodoList";
 
 function App() {
   return (
-    <TodoList />
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </>
   );
 }
 
