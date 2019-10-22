955cf9c72cd381eac20c9130894ccb7827928cea
beata <beata@emc2wave.com>
2019-10-22 16:59:46 -0700

    add event onClick func useRef

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
   const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    e.preventDefult();
+
+
+  }
+
   return (
     <>
       <TodoList todos={todos}/>
-      <input type="text" />
-      <button>Add Todo</button>
+      <input type="text" ref={todoNameRef}/>
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 left to do</div>
     </>
