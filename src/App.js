3a97a92e48134a12dd4aeff2d1801d9a6ec940d3
beata <beata@emc2wave.com>
2019-10-22 17:24:58 -0700

    add localstorege

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
 import uuidv4 from 'uuid/v4'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
 
   function handleAddTodo(e) {
     
